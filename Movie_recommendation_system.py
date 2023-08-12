44c3c970a935b5ffa385fc4dd045bc398856295e
Tushar Sethi <104630257+tushar151@users.noreply.github.com>
2023-08-12 10:00:14 +0530

    Add files via upload
    This Movie Recommendation Project is used to recommend similar movies related to the movie given by the user.

---
+++ Movie_recommendation_system.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import streamlit as st
+import pickle
+import requests
+
+
+def fetch_poster(movie_id):
+    response=requests.get(
+        'https://api.themoviedb.org/3/movie/{}?api_key=46ba99765bd9c8730449453dd57eb929&language=en-US%27'.format(
+            movie_id))
+    data = response.json()
+
+
+
+    return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
+
+
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distance = similarity[movie_index]
+    movies_list = sorted(list(enumerate(distance)), reverse=True, key=lambda x: x[1])[1:6]
+    recommended_movies = []
+    recommended_movies_posters = []
+
+    for i in movies_list:
+        movie_id = movies.iloc[i[0]].movie_id
+        # fetch poster from api
+
+        recommended_movies.append(movies.iloc[i[0]].title)
+        recommended_movies_posters.append(fetch_poster(movie_id))
+    return recommended_movies, recommended_movies_posters
+
+
+movies_dict = pickle.load(open('movies_dict.pkl', 'rb'))
+movies = pd.DataFrame(movies_dict)
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+st.title('Movie Recommender System')
+selected_movie_name = st.selectbox('What movie do you want to search?',movies['title'].values)
+
+if st.button("RECOMMEND"):
+
+    names, posters = recommend(selected_movie_name)
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.text(names[0])
+        st.image(posters[0])
+    with col2:
+        st.text(names[1])
+        st.image(posters[1])
+    with col3:
+        st.text(names[2])
+        st.image(posters[2])
+    with col4:
+        st.text(names[3])
+        st.image(posters[3])
+    with col5:
+        st.text(names[4])
+        st.image(posters[4])
